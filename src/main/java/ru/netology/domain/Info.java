d986f39463489cfe96dcab9bf51f3704ee60a4ff
NDmitrieva <al_amanecer@mail.ru>
2021-11-05 23:09:10 +0300

    added Class

---
+++ src/main/java/ru/netology/domain/Info.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+public class Info {
+    private String text;
+    private String imageUrl;
+    private String fav;
+    private String buttonRead;
+    private String like;
+    private String comment;
+    private String share;
+    private String view;
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,9 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private String logoUrl;
+    private String author;
+    private int date;
+    private String menu;
+}
